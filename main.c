9c6dae9379141b6e2236996c1c7b7e665c93114c
matheus luiz <santaremmatheusluiz@gmail.com>
2026-09-16 01:06:34 -0400

    adicionando o algoritmo insertion sort no arquivo main.c

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+void insertion_sort(int arr[], int n) {
+    int i, chave, j;
+
+    for (i = 1; i < n; i++) {
+        chave = arr[i];
+        j = i - 1;
+
+        // Move os elementos maiores que 'chave' uma posição à frente
+        while (j >= 0 && arr[j] > chave) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = chave;
+    }
+}
+
+void imprimir(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 int main(void) {
 
     printf("Oi, mundo");
